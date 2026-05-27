66551bc4e42bf7dd69dbe77f76b762cdbfabe641
Raknage <68710615+Raknage@users.noreply.github.com>
2026-05-28 01:23:21 +0300

    Preact component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,19 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://dynamic-alfajores-a67778.netlify.app/",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   image: {
     domains: ["astro.build"],
   },
+
+  integrations: [preact()],
 });
