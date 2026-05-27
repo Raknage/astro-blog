5bd5b4bb57ad44d042be13cfb090e756f779d567
Raknage <68710615+Raknage@users.noreply.github.com>
2026-05-27 20:30:57 +0300

    rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://dynamic-alfajores-a67778.netlify.app/",
   vite: {
     plugins: [tailwindcss()],
   },
